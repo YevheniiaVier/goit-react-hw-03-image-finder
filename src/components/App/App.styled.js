b8558b32054072c6d056e88074155a511d707f25
YevheniiaVier <gotashk1983@gmail.com>
2023-01-18 00:57:40 +0200

    adds searchbar render

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -1,6 +1,8 @@
-// .App {
-//   display: grid;
-//   grid-template-columns: 1fr;
-//   grid-gap: 16px;
-//   padding-bottom: 24px;
-// }
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;
+`;
