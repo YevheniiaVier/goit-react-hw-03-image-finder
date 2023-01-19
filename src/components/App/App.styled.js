d488b3b5d5db8428104a752563c0c6833fff2f80
YevheniiaVier <gotashk1983@gmail.com>
2023-01-19 22:32:26 +0200

    corrects styled

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -4,6 +4,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 16px;
   // grid-template-columns: 1fr;
   // grid-gap: 16px;
   // padding-bottom: 24px;
