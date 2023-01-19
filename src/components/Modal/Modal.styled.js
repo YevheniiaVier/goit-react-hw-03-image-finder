d4666bdd2298b091b7eb51821d400c46340cfc33
YevheniiaVier <gotashk1983@gmail.com>
2023-01-20 01:31:42 +0200

    corrects modal styles

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -14,44 +14,14 @@ export const Backdrop = styled.div`
 `;
 
 export const ModalContent = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  min-height: 380px;
-  max-width: 600px;
-  width: 100%;
-  padding: 12px;
-  background-color: #fff;
-  border-radius: 3px;
-  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
-    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
-  display: flex;
-  justify-content: center;
   max-width: calc(100vw - 48px);
   max-height: calc(100vh - 24px);
 `;
-
-// .Overlay {
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   width: 100vw;
-//   height: 100vh;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   background-color: rgba(0, 0, 0, 0.8);
-//   z-index: 1200;
-// }
-
-// .Modal {
-
-// }
 
 export const LargeImage = styled.img.attrs(props => ({
   src: props.src,
   alt: props.alt,
 }))`
   width: 100%;
+  object-fit: cover;
 `;
